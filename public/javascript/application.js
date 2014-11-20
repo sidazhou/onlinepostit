d09e0d296ca9b58ee1111ff63673a2b960b7cee9
kjolli <krish.jolliffe@gmail.com>
2014-11-20 04:33:29 +0000

    resizable and dragable

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,5 +1,12 @@
-$(document).ready(function() {
+//Adds new note to page when add tab is clicked
+function addNote(){
+  var $newNote = $('<div class="resize draggable" style="width: 200px; height: 200px;"></div>');
+  $newNote.appendTo('.resize-container');
+};
 
+  /* Post It Moveability */
+
+$(document).ready(function() {
     interact('.resize')
     .resizable(true)
     .on('resizemove', function (event) {
@@ -16,6 +23,50 @@ $(document).ready(function() {
 
       target.textContent = newWidth + '×' + newHeight;
     });
+
+    // target elements with the "draggable" class
+interact('.draggable')
+    .draggable({
+        // allow dragging of multple elements at the same time
+        max: Infinity,
+
+        // call this function on every dragmove event
+        onmove: function (event) {
+            var target = event.target,
+                // keep the dragged position in the data-x/data-y attributes
+                x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+                y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+            // translate the element
+            target.style.webkitTransform =
+            target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)';
+
+            // update the posiion attributes
+            target.setAttribute('data-x', x);
+            target.setAttribute('data-y', y);
+        },
+        // call this function on every dragend event
+        onend: function (event) {
+            var textEl = event.target.querySelector('p');
+            
+            textEl && (textEl.textContent =
+                'moved a distance of '
+                + (Math.sqrt(event.dx * event.dx +
+                             event.dy * event.dy)|0) + 'px');
+        }
+    })
+    // enable inertial throwing
+    .inertia(true)
+    // keep the element within the area of it's parent
+    .restrict({
+        drag: "parent",
+        endOnly: true,
+        elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+    });
+
+    // allow more than one interaction at a time
+    interact.maxInteractions(Infinity);
 
   /*  Navigation  */
 
