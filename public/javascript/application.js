0ad27c45ec22ba21c89124c0d2c3fa058d91c8bb
Regan Perkins <reganlperkins@gmail.com>
2014-11-20 01:52:06 +0000

    add drop down menu to settings icon

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,22 +1,28 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    interact('.resize')
+    .resizable(true)
+    .on('resizemove', function (event) {
+      var target = event.target;
 
-  interact('.resize')
-  .resizable(true)
-  .on('resizemove', function (event) {
-    var target = event.target;
+      // add the change in coords to the previous width of the target element
+      var
+        newWidth  = parseFloat(target.style.width ) + event.dx,
+        newHeight = parseFloat(target.style.height) + event.dy;
 
-    // add the change in coords to the previous width of the target element
-    var
-      newWidth  = parseFloat(target.style.width ) + event.dx,
-      newHeight = parseFloat(target.style.height) + event.dy;
+      // update the element's style
+      target.style.width  = newWidth + 'px';
+      target.style.height = newHeight + 'px';
 
-    // update the element's style
-    target.style.width  = newWidth + 'px';
-    target.style.height = newHeight + 'px';
+      target.textContent = newWidth + '×' + newHeight;
+    });
 
-    target.textContent = newWidth + '×' + newHeight;
+  /*  Navigation  */
+
+  $(function() {
+      $('#settings').click(function(){
+        $('.submenu').slideToggle();
+      });
   });
 
 });
