3ac07e2b3ca56a757bf8df0beb7d149fdeb65f0c
Regan Perkins <reganlperkins@gmail.com>
2014-11-20 00:23:46 +0000

    add a resizable test div

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,22 @@
 $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+  interact('.resize')
+  .resizable(true)
+  .on('resizemove', function (event) {
+    var target = event.target;
+
+    // add the change in coords to the previous width of the target element
+    var
+      newWidth  = parseFloat(target.style.width ) + event.dx,
+      newHeight = parseFloat(target.style.height) + event.dy;
+
+    // update the element's style
+    target.style.width  = newWidth + 'px';
+    target.style.height = newHeight + 'px';
+
+    target.textContent = newWidth + '×' + newHeight;
+  });
+
 });
